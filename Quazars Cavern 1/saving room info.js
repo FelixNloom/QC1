126e7dea3ddfe926f54cea6d4f44f727f506f66e
FelixNloom <felixnloom@gmail.com>
2018-06-05 00:04:17 -0400

    removed some garbage. im reworking the game

---
+++ Quazars Cavern 1/saving room info.js
@@ -0,0 +1,136 @@
+if ($gameVariables.value(23) == 8)
+	{
+		if ($gameVariables.value(24) == 8)
+		{
+			
+			//room save
+			$gameVariables.setValue(198, $gameVariables.value(1));
+			//mon save 0 = no mon
+			$gameVariables.setValue(199, $gameVariables.value(2));
+			//loot save 0 = no loot
+			$gameVariables.setValue(200, $gameVariables.value(3));
+		}
+		else if ($gameVariables.value(24) == 7)
+		{
+			
+		}
+
+		else if ($gameVariables.value(24) == 6)
+		{
+			
+		}
+		
+		else if ($gameVariables.value(24) == 5)
+		{
+			
+		}
+		else if ($gameVariables.value(24) == 4)
+		{
+			
+		}
+	
+	} 
+else if ($gameVariables.value(23) == 7)
+	{
+		if ($gameVariables.value(24) == 8)
+		{
+			
+		}
+		else if ($gameVariables.value(24) == 7)
+		{
+			
+		}
+
+		else if ($gameVariables.value(24) == 6)
+		{
+			
+		}
+		
+		else if ($gameVariables.value(24) == 5)
+		{
+			
+		}
+		else if ($gameVariables.value(24) == 4)
+		{
+			
+		}
+	
+	} 
+else if ($gameVariables.value(23) == 6)
+	{
+		if ($gameVariables.value(24) == 8)
+		{
+			
+		}
+		else if ($gameVariables.value(24) == 7)
+		{
+			
+		}
+
+		else if ($gameVariables.value(24) == 6)
+		{
+			
+		}
+		
+		else if ($gameVariables.value(24) == 5)
+		{
+			
+		}
+		else if ($gameVariables.value(24) == 4)
+		{
+			
+		}
+	
+	} 
+	else if ($gameVariables.value(23) == 5)
+	{
+		if ($gameVariables.value(24) == 8)
+		{
+			
+		}
+		else if ($gameVariables.value(24) == 7)
+		{
+			
+		}
+
+		else if ($gameVariables.value(24) == 6)
+		{
+			
+		}
+		
+		else if ($gameVariables.value(24) == 5)
+		{
+			
+		}
+		else if ($gameVariables.value(24) == 4)
+		{
+			
+		}
+	
+	} 
+	else if ($gameVariables.value(23) == 4)
+	{
+		if ($gameVariables.value(24) == 8)
+		{
+			
+		}
+		else if ($gameVariables.value(24) == 7)
+		{
+			
+		}
+
+		else if ($gameVariables.value(24) == 6)
+		{
+			
+		}
+		
+		else if ($gameVariables.value(24) == 5)
+		{
+			
+		}
+		else if ($gameVariables.value(24) == 4)
+		{
+			
+		}
+	
+	} 
