b7db27be334393311d7f09581153efa5f7183846
FelixNloom <felixnloom@gmail.com>
2018-06-14 16:00:55 -0400

    14 June 2018 Big Updates
    changed a bunch of shit. I basically rewrote everything. I used a lot of Javascript this time.

--- qc_rebuild/js/plugins/fun with orfices.js
+++ qc_rebuild/js/plugins/fun with orfices.js
@@ -2,3 +2,29 @@ function printMonName(mon)
 	{
 		$gameMessage.add("OH GOD THERE IS " + mon +" HERE!");
 	}
+	
+function dsCheck()
+	{
+		if ($gameVariables.value(1) == $gameVariables.value(37))
+		{
+			if ($gameVariables.value(2) == $gameVariables.value(38))
+			{
+				$gameMessage.add("There's a stairway down here...");
+				$gameSwitches.setValue(5, true);
+			} 
+		} 
+	}
+	
+function keyCheck()
+	{
+		if ($gameVariables.value(1) == $gameVariables.value(39))
+		{
+			if ($gameVariables.value(2) == $gameVariables.value(40))
+			{
+				$gameMessage.add("Hey, there's a key here!");
+				$gameSwitches.setValue(6, true);
+			} 
+		} 
+	}
+	
+	
--- qc_rebuild/js/plugins/where to save function.js
+++ qc_rebuild/js/plugins/where to save function.js
@@ -37,132 +37,132 @@ else if ($gameVariables.value(1) == 7)
 	{
 		if ($gameVariables.value(2) == 8)
 		{
-			$gameVariables.setValue(73, $gameVariables.value(3)); //saves room
-			$gameVariables.setValue(74, $gameVariables.value(4)); //saves monster
-			$gameVariables.setValue(75, $gameVariables.value(5)); //saves loot
+			$gameVariables.setValue(76, $gameVariables.value(3)); //saves room
+			$gameVariables.setValue(77, $gameVariables.value(4)); //saves monster
+			$gameVariables.setValue(78, $gameVariables.value(5)); //saves loot
 		}
 		else if ($gameVariables.value(2) == 7)
 		{
-			$gameVariables.setValue(75, $gameVariables.value(3)); //saves room
-			$gameVariables.setValue(76, $gameVariables.value(4)); //saves monster
-			$gameVariables.setValue(77, $gameVariables.value(5)); //saves loot
+			$gameVariables.setValue(79, $gameVariables.value(3)); //saves room
+			$gameVariables.setValue(80, $gameVariables.value(4)); //saves monster
+			$gameVariables.setValue(81, $gameVariables.value(5)); //saves loot
 		}
 		else if ($gameVariables.value(2) == 6)
 		{
-			$gameVariables.setValue(78, $gameVariables.value(3)); //saves room
-			$gameVariables.setValue(79, $gameVariables.value(4)); //saves monster
-			$gameVariables.setValue(80, $gameVariables.value(5)); //saves loot
+			$gameVariables.setValue(82, $gameVariables.value(3)); //saves room
+			$gameVariables.setValue(83, $gameVariables.value(4)); //saves monster
+			$gameVariables.setValue(84, $gameVariables.value(5)); //saves loot
 		}
 		else if ($gameVariables.value(2) == 5)
 		{
-			$gameVariables.setValue(81, $gameVariables.value(3)); //saves room
-			$gameVariables.setValue(81, $gameVariables.value(4)); //saves monster
-			$gameVariables.setValue(83, $gameVariables.value(5)); //saves loot
+			$gameVariables.setValue(85, $gameVariables.value(3)); //saves room
+			$gameVariables.setValue(86, $gameVariables.value(4)); //saves monster
+			$gameVariables.setValue(87, $gameVariables.value(5)); //saves loot
 		}
 		else if ($gameVariables.value(2) == 4)
 		{
-			$gameVariables.setValue(84, $gameVariables.value(3)); //saves room
-			$gameVariables.setValue(85, $gameVariables.value(4)); //saves monster
-			$gameVariables.setValue(86, $gameVariables.value(5)); //saves loot
+			$gameVariables.setValue(88, $gameVariables.value(3)); //saves room
+			$gameVariables.setValue(89, $gameVariables.value(4)); //saves monster
+			$gameVariables.setValue(90, $gameVariables.value(5)); //saves loot
 		}	
 	}
 else if ($gameVariables.value(1) == 6)
 	{
 		if ($gameVariables.value(2) == 8)
 		{
-			$gameVariables.setValue(87, $gameVariables.value(3)); //saves room
-			$gameVariables.setValue(88, $gameVariables.value(4)); //saves monster
-			$gameVariables.setValue(89, $gameVariables.value(5)); //saves loot
+			$gameVariables.setValue(91, $gameVariables.value(3)); //saves room
+			$gameVariables.setValue(92, $gameVariables.value(4)); //saves monster
+			$gameVariables.setValue(93, $gameVariables.value(5)); //saves loot
 		}
 		else if ($gameVariables.value(2) == 7)
 		{
-			$gameVariables.setValue(90, $gameVariables.value(3)); //saves room
-			$gameVariables.setValue(91, $gameVariables.value(4)); //saves monster
-			$gameVariables.setValue(92, $gameVariables.value(5)); //saves loot
+			$gameVariables.setValue(94, $gameVariables.value(3)); //saves room
+			$gameVariables.setValue(95, $gameVariables.value(4)); //saves monster
+			$gameVariables.setValue(96, $gameVariables.value(5)); //saves loot
 		}
 		else if ($gameVariables.value(2) == 6)
 		{
-			$gameVariables.setValue(93, $gameVariables.value(3)); //saves room
-			$gameVariables.setValue(93, $gameVariables.value(4)); //saves monster
-			$gameVariables.setValue(95, $gameVariables.value(5)); //saves loot
+			$gameVariables.setValue(97, $gameVariables.value(3)); //saves room
+			$gameVariables.setValue(98, $gameVariables.value(4)); //saves monster
+			$gameVariables.setValue(99, $gameVariables.value(5)); //saves loot
 		}
 		else if ($gameVariables.value(2) == 5)
-		{
-			$gameVariables.setValue(96, $gameVariables.value(3)); //saves room
-			$gameVariables.setValue(97, $gameVariables.value(4)); //saves monster
-			$gameVariables.setValue(98, $gameVariables.value(5)); //saves loot
-		}
-		else if ($gameVariables.value(99) == 4)
 		{
 			$gameVariables.setValue(100, $gameVariables.value(3)); //saves room
 			$gameVariables.setValue(101, $gameVariables.value(4)); //saves monster
 			$gameVariables.setValue(102, $gameVariables.value(5)); //saves loot
+		}
+		else if ($gameVariables.value(99) == 4)
+		{
+			$gameVariables.setValue(103, $gameVariables.value(3)); //saves room
+			$gameVariables.setValue(104, $gameVariables.value(4)); //saves monster
+			$gameVariables.setValue(105, $gameVariables.value(5)); //saves loot
 		}	
 	}
 else if ($gameVariables.value(1) == 5)
 	{
 		if ($gameVariables.value(2) == 8)
 		{
-			$gameVariables.setValue(103, $gameVariables.value(3)); //saves room
-			$gameVariables.setValue(104, $gameVariables.value(4)); //saves monster
-			$gameVariables.setValue(105, $gameVariables.value(5)); //saves loot
-		}
-		else if ($gameVariables.value(2) == 7)
-		{
 			$gameVariables.setValue(106, $gameVariables.value(3)); //saves room
 			$gameVariables.setValue(107, $gameVariables.value(4)); //saves monster
 			$gameVariables.setValue(108, $gameVariables.value(5)); //saves loot
 		}
-		else if ($gameVariables.value(2) == 6)
+		else if ($gameVariables.value(2) == 7)
 		{
 			$gameVariables.setValue(109, $gameVariables.value(3)); //saves room
 			$gameVariables.setValue(110, $gameVariables.value(4)); //saves monster
 			$gameVariables.setValue(111, $gameVariables.value(5)); //saves loot
 		}
-		else if ($gameVariables.value(2) == 5)
+		else if ($gameVariables.value(2) == 6)
 		{
 			$gameVariables.setValue(112, $gameVariables.value(3)); //saves room
 			$gameVariables.setValue(113, $gameVariables.value(4)); //saves monster
 			$gameVariables.setValue(114, $gameVariables.value(5)); //saves loot
 		}
-		else if ($gameVariables.value(2) == 4)
+		else if ($gameVariables.value(2) == 5)
 		{
 			$gameVariables.setValue(115, $gameVariables.value(3)); //saves room
-			$gameVariables.setValue(115, $gameVariables.value(4)); //saves monster
+			$gameVariables.setValue(116, $gameVariables.value(4)); //saves monster
 			$gameVariables.setValue(117, $gameVariables.value(5)); //saves loot
+		}
+		else if ($gameVariables.value(2) == 4)
+		{
+			$gameVariables.setValue(118, $gameVariables.value(3)); //saves room
+			$gameVariables.setValue(119, $gameVariables.value(4)); //saves monster
+			$gameVariables.setValue(120, $gameVariables.value(5)); //saves loot
 		}	
 	}
 else if ($gameVariables.value(1) == 4)
 	{
 		if ($gameVariables.value(2) == 8)
 		{
-			$gameVariables.setValue(118, $gameVariables.value(3)); //saves room
-			$gameVariables.setValue(119, $gameVariables.value(4)); //saves monster
-			$gameVariables.setValue(120, $gameVariables.value(5)); //saves loot
-		}
-		else if ($gameVariables.value(2) == 7)
-		{
 			$gameVariables.setValue(121, $gameVariables.value(3)); //saves room
 			$gameVariables.setValue(122, $gameVariables.value(4)); //saves monster
 			$gameVariables.setValue(123, $gameVariables.value(5)); //saves loot
 		}
-		else if ($gameVariables.value(2) == 6)
+		else if ($gameVariables.value(2) == 7)
 		{
 			$gameVariables.setValue(124, $gameVariables.value(3)); //saves room
 			$gameVariables.setValue(125, $gameVariables.value(4)); //saves monster
 			$gameVariables.setValue(126, $gameVariables.value(5)); //saves loot
 		}
-		else if ($gameVariables.value(2) == 5)
+		else if ($gameVariables.value(2) == 6)
 		{
 			$gameVariables.setValue(127, $gameVariables.value(3)); //saves room
 			$gameVariables.setValue(128, $gameVariables.value(4)); //saves monster
 			$gameVariables.setValue(129, $gameVariables.value(5)); //saves loot
 		}
-		else if ($gameVariables.value(2) == 4)
+		else if ($gameVariables.value(2) == 5)
 		{
 			$gameVariables.setValue(130, $gameVariables.value(3)); //saves room
 			$gameVariables.setValue(131, $gameVariables.value(4)); //saves monster
 			$gameVariables.setValue(132, $gameVariables.value(5)); //saves loot
+		}
+		else if ($gameVariables.value(2) == 4)
+		{
+			$gameVariables.setValue(133, $gameVariables.value(3)); //saves room
+			$gameVariables.setValue(134, $gameVariables.value(4)); //saves monster
+			$gameVariables.setValue(135, $gameVariables.value(5)); //saves loot
 		}	
 	}
 }	
